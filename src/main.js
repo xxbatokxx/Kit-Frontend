23988f63099798ed304fb062e0014d007149d2ab
xxbatokxx <iqbalrisqi700@gmail.com>
2023-07-07 23:12:05 +0700

    view homePage

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-import App from './App.vue'
+import App from "./App.vue"
 import router from './router'
 
-import './assets/main.css'
 
 const app = createApp(App)
 
