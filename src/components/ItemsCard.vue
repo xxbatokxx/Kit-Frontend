3aace8ac1589e5d0d81cf8be609180e7557c54f5
xxbatokxx <iqbalrisqi700@gmail.com>
2023-07-14 15:12:58 +0700

    comit

---
+++ src/components/ItemsCard.vue
@@ -0,0 +1,71 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+
+defineProps({
+    title: String,
+    desc: String,
+    image: String
+})
+</script>
+
+<template>
+    <div class="w-full px-1 my-1 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+        <div class="overflow-hidden border border-gray-200 rounded-xl">
+            <RouterLink to="/">
+                <div class="m-4 overflow-hidden rounded-xl">
+                    <img :alt="image" class="block w-full h-auto" :src="'/src/assets/img/' + image" />
+                </div>
+            </RouterLink>
+            <header class="px-4 mb-4 leading-tight">
+                <h1 class="text-lg">
+                    <RouterLink class="font-semibold text-black no-underline hover:underline" to="/">
+                        {{ title }}
+                    </RouterLink>
+                </h1>
+                <span class="block text-sm font-light text-gray-500 no-underline">
+                    {{ desc }}
+                </span>
+            </header>
+        </div>
+    </div>
+    <!-- <div class="w-full px-1 my-1 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+        <div class="overflow-hidden border border-gray-200 rounded-xl">
+            <a href="details.html">
+                <div class="m-4 overflow-hidden rounded-xl">
+                    <img alt="Placeholder" class="block w-full h-auto" src="@/assets/img/items-2.jpg" />
+                </div>
+            </a>
+
+            <header class="px-4 mb-4 leading-tight">
+                <h1 class="text-lg">
+                    <a class="font-semibold text-black no-underline hover:underline" href="#">
+                        Online Doctor Consultation
+                    </a>
+                </h1>
+                <span class="block text-sm font-light text-gray-500 no-underline">
+                    Website UI Kit
+                </span>
+            </header>
+        </div>
+    </div>
+    <div class="w-full px-1 my-1 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+        <div class="overflow-hidden border border-gray-200 rounded-xl">
+            <a href="details.html">
+                <div class="m-4 overflow-hidden rounded-xl">
+                    <img alt="Placeholder" class="block w-full h-auto" src="@/assets/img/items-3.jpg" />
+                </div>
+            </a>
+
+            <header class="px-4 mb-4 leading-tight">
+                <h1 class="text-lg">
+                    <a class="font-semibold text-black no-underline hover:underline" href="#">
+                        Banking Crypto
+                    </a>
+                </h1>
+                <span class="block text-sm font-light text-gray-500 no-underline">
+                    Mobile UI Kit
+                </span>
+            </header>
+        </div>
+    </div> -->
+</template>
---
+++ src/components/homepage/Items.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from 'vue';
+import ItemsCard from '../ItemsCard.vue';
+
+const items = ref([
+    {id: 1, title: 'Mobile UI Kit', desc: 'Mobile UI Kit', image: 'items-1.jpg'},
+    {id: 2, title: 'Online Doctor Consultation', desc: 'Website UI Kit', image: 'items-2.jpg'},
+    {id: 3, title: 'Banking Crypto', desc: 'Mobile UI Kit', image: 'items-3.jpg'}
+])
+</script>
+
+<template>
+    <div class="container px-4 mx-auto my-16 md:px-12">
+        <h2 class="mb-4 text-xl font-medium md:mb-0 md:text-lg">New Items</h2>
+        <div class="flex flex-wrap -mx-1 lg:-mx-4">
+            <ItemsCard
+            v-for="Itemscard in items"
+            :key="Itemscard.id"
+            :title="Itemscard.title"
+            :desc="Itemscard.desc"
+            :image="Itemscard.image"
+            />
+        </div>
+    </div>
+</template>
